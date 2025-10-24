92d0e37018f2f35d005cc0a281abb5f322b8c1a4
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2025-10-23 18:39:40 -0700

    Finished q1, started q2

--- q2.js
+++ q2.js
@@ -1,6 +1,9 @@
 // 2. Find the largest number in an array (using a loop)
 function findLargest(array) {
-
+  let max = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= )
+  }
 }
 
 // Test for find largest
